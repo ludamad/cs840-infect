ad1f4114cbec4f489278815117cc757f48754f67
Adam <domuradical@gmail.com>
2014-11-06 18:27:15 -0500

    Setup

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,29 @@ using namespace std;
 
 typedef int entity_id;
 
+/*****************************************************************************
+ *
+ *****************************************************************************/
+
+struct SelectDS {
+
+};
+
+/*****************************************************************************
+ *
+ *****************************************************************************/
+
+struct Connection {
+	entity_id entity;
+	double transmission_probability;
+	Connection(entity_id entity, double transmission_probability) {
+		this->entity = entity;
+		this->transmission_probability = transmission_probability;
+	}
+};
+
 struct Entity {
-    vector<entity_id> entities;
+    vector<Connection> influence_set;
 };
 
 struct Settings {
@@ -17,36 +38,49 @@ struct Settings {
     string method = "bad";
     int seed = 0xBADF00D;
     size_t size = 10;
+    double transmission_prob = 0.1;
     double connectivity = 0.1;
 };
 
-struct Network {
+struct State {
 	MTwist rng;
     vector<Entity> entities;
+    double time = 0;
+
     size_t size() {
     	return entities.size();
     }
     void init(const Settings& settings);
+    void step();
 };
 
-void Network::init(const Settings& S) {
+/*****************************************************************************
+ * Implementations
+ *****************************************************************************/
+
+void State::init(const Settings& S) {
 	rng.init_genrand(S.seed);
 	entities = vector<Entity>(S.size);
+	time = 0;
 	for (Entity& e : entities) {
 		for (entity_id e_id = 0; e_id < size(); e_id++) {
-			if (&e == &entities[e_id]) {
+			if (&e != &entities[e_id]) {
 				// For every node other than ourselves, connect with 'connectivity' probability
 				if (rng.rand_real_not1() < S.connectivity) {
-					e.entities.push_back(e_id);
+					e.influence_set.push_back({e_id, S.transmission_prob});
 				}
 			}
 		}
 	}
 }
 
+/*****************************************************************************
+ * Test driver
+ *****************************************************************************/
+
 int main(int argn, char** args) {
 	Settings settings;
-	Network network;
+	State network;
 	// Create the network according to passed settings
 	network.init(settings);
     return 0;
