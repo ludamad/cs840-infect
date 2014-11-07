c8eb8141c244f33af0bb7c854dd8b6e15b49b57d
Adam <domuradical@gmail.com>
2014-11-07 02:50:16 -0500

    Bunch of work

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ set +x
 mkdir -p build/ 
 cd build 
 cmake -DCMAKE_BUILD_TYPE=Debug ../src
-make -j5
-./infectsim
+make -j5 && cd ../src && gdb -silent -ex=r -ex=q ../build/infectsim
--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,90 @@
 #include <cstdio>
 #include <string>
+#include <sstream>
 #include <vector>
 
-#include "hashkat-utils/mtwist.h"
+#include "libs/unittest.h"
+
+#include "sdl.h"
+#include "network.h"
 
 using namespace std;
-
-typedef int entity_id;
-
-/*****************************************************************************
- *
- *****************************************************************************/
-
-struct SelectDS {
-
-};
-
-/*****************************************************************************
- *
- *****************************************************************************/
-
-struct Connection {
-	entity_id entity;
-	double transmission_probability;
-	Connection(entity_id entity, double transmission_probability) {
-		this->entity = entity;
-		this->transmission_probability = transmission_probability;
-	}
-};
-
-struct Entity {
-    vector<Connection> influence_set;
-};
-
-struct Settings {
-    string graph_type = "uniform";
-    string method = "bad";
-    int seed = 0xBADF00D;
-    size_t size = 10;
-    double transmission_prob = 0.1;
-    double connectivity = 0.1;
-};
-
-struct State {
-	MTwist rng;
-    vector<Entity> entities;
-    double time = 0;
-
-    size_t size() {
-    	return entities.size();
-    }
-    void init(const Settings& settings);
-    void step();
-};
-
-/*****************************************************************************
- * Implementations
- *****************************************************************************/
-
-void State::init(const Settings& S) {
-	rng.init_genrand(S.seed);
-	entities = vector<Entity>(S.size);
-	time = 0;
-	for (Entity& e : entities) {
-		for (entity_id e_id = 0; e_id < size(); e_id++) {
-			if (&e != &entities[e_id]) {
-				// For every node other than ourselves, connect with 'connectivity' probability
-				if (rng.rand_real_not1() < S.connectivity) {
-					e.influence_set.push_back({e_id, S.transmission_prob});
-				}
-			}
-		}
-	}
-}
 
 /*****************************************************************************
  * Test driver
  *****************************************************************************/
 
+void output_network(string label, int dim, State& network) {
+
+	static vector<bool> wasInfected;
+	static bool SDL_INITIALIZED = false;
+
+	static unsigned int COL_BLACK = 0;
+	static unsigned int COL_WHITE = 0xFFFFFFFF;
+	static unsigned int COL_GREY = 0xFF666699;
+	static unsigned int COL_RED1 = 0xFFFF0000;
+	static unsigned int COL_RED2 = 0xFF990000;
+	static unsigned int COL_ORANGE = 0xFFFF9900;
+
+	int scale = 640 / dim;
+	if (!SDL_INITIALIZED) {
+		wasInfected.resize(network.size(), false);
+		sdl_init(scale * dim, scale * dim);
+		SDL_INITIALIZED = true;
+	}
+	sdl_predraw();
+	int i = 0;
+	for (int y = 0; y < dim; y++) {
+		for (int x = 0; x < dim; x++) {
+			auto& e = network.entities.at(i);
+			unsigned int colour = COL_WHITE;
+			if (e.infected) {
+				colour = COL_ORANGE;
+				if (!wasInfected[i]) {
+					colour = COL_RED1;
+					wasInfected[i] = true;
+				}
+			}
+			// TODO: Simplify for large networks:
+			sdl_fill_rect(x * scale, y * scale, scale, scale, COL_BLACK);
+			sdl_fill_rect(x * scale, y * scale, scale - 1, scale - 1, colour);
+			i++;
+		}
+	}
+	stringstream ss(label);
+	char buff[500];
+	int y = 30;
+	while (!ss.eof()) {
+		ss.getline(buff, 500);
+		sdl_draw_text(buff, 30, y);
+		y += 30;
+	}
+	sdl_postdraw();
+}
+
 int main(int argn, char** args) {
-	Settings settings;
-	State network;
-	// Create the network according to passed settings
-	network.init(settings);
+    run_unittests();
+    int dim = 100, n_start = 10;
+    Settings settings(dim*dim, 1, 0xBADF00D);
+    State network;
+    // Create the network according to passed settings
+    network.init(settings);
+    for (int i = 0; i < n_start; i++) {
+		sdl_delay(100);
+		network.infect_n_random(1);
+		stringstream ss;
+		ss << i;
+		output_network("Seeding Infection #" + ss.str(), dim, network);
+    }
+    while (true) {
+		sdl_delay(100);
+		network.step();
+		stringstream ss("Simulation ");
+		ss << "W = " << network.active_infections.total_weight() << endl;
+		ss << ", sim-time =" << network.time_elapsed * 1000 << "ms" << endl;
+		ss << "step: " << network.n_steps;
+		ss << "infections: " << network.n_infections;
+		output_network(ss.str(), dim, network);
+    }
     return 0;
 }
